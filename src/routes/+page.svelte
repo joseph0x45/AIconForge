f9d8798fd32a3862d5a24ce123e67cd6a3be540b
TheWisePigeon <zozozozeph@gmail.com>
2023-02-18 22:58:30 +0000

    ui updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,15 @@
-<h1 class=" text-2xl" >Welcome to AIconForge</h1>
+<main class=" p-3" >
+    <header class=" flex justify-between items-center" >
+        <h1 class=" font-bold text-lg bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-green-300 via-blue-500 to-purple-600 bg-clip-text text-transparent" >
+            AIconForge
+        </h1>
+        <div>
+            <button class=" text-white bg-gradient-to-l from-green-300 via-blue-500 to-purple-600 font-bold w-28 rounded-3xl p-2" >
+                Login
+            </button>
+        </div>
+    </header>
+    <div class=" text-2xl font-extrabold text-center text-white" >
+        <h1>AIconForge is an AI powered icon generator</h1>
+    </div>
+</main>
