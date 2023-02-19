93d5533538842f9ef3d56d874eec57e7bd639a36
TheWisePigeon <zozozozeph@gmail.com>
2023-02-19 23:24:33 +0000

    more ui updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,13 @@
 <main class=" p-3" >
-    <header class=" flex justify-between items-center" >
-        <h1 class=" font-bold text-lg bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-green-300 via-blue-500 to-purple-600 bg-clip-text text-transparent" >
+    <header class=" flex justify-between items-baseline " >
+        <h1 class=" font-extrabold text-xl bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-green-300 via-blue-500 to-purple-600 bg-clip-text text-transparent" >
             AIconForge
         </h1>
-        <div>
-            <a href="#auth">
-                <h1 class=" text-transparent underline decoration-wavy decoration-white bg-clip-text bg-gradient-to-l from-green-300 via-blue-500 to-purple-600 font-bold" >
-                    Get Started
-                </h1>
-            </a>
-        </div>
+        <a class=" decoration-white decoration-wavy text-transparent underline bg-clip-text bg-gradient-to-l from-green-300 via-blue-500 to-purple-600 font-bold" href="#auth">
+            Get Started
+        </a>
     </header>
-    <div class=" text-2xl font-extrabold text-center text-white" >
-        <h1>AIconForge is an AI powered icon generator</h1>
+    <div class=" text-2xl font-extrabold text-center text-white pt-28" >
+        <h1>Boost your brand with custom unique icons created by AI</h1>
     </div>
 </main>
